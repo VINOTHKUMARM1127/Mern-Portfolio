c6077d9f33467f5f2258b7326bd061b2ddbbe24b
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-10-08 22:57:21 +0530

    add frontend for projects Route

--- Frontend/src/Admin/Login.jsx
+++ Frontend/src/Admin/Login.jsx
@@ -1,26 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <div className='flex justify-center items-center min-h-[89vh] wapp'>
-        <div className=" w-[80%] md:w-[600px] rounded-lg text-white">
-        <form onSubmit="" className='flex flex-col w-[70%] md:w-[300px] mx-auto mb-10 p-5 '>
-            <div className='text-center text-[1.7em] font-semibold mb-4'>LOGIN</div>
-            <div className='mb-[1%]'>Email</div>
-            <input onChange={e=>{setEmail(e.target.value)}} className='text-black px-2 py-1 mb-5 rounded-sm bg-opacity-0' type="mail" value={email} placeholder='Enter Email'  />
-            <div className='mb-[1%]'>Password</div>
-            <input onChange={e=>{setPassword(e.target.value)}} className='text-black px-2 py-1 rounded-sm bg-none' type="text" value={password} placeholder='Enter Password'  />
-            <div className='my-2 text-[0.8em] hover:text-blue-700'>Forget Password?</div>
-            <button type="submit" className='bg-blue-500 py-2 rounded-md'>Login</button>
+    <div className="flex justify-center items-center min-h-[89vh] wapp">
+      <div className=" w-[80%] md:w-[600px] rounded-lg text-white">
+        <form
+          onSubmit=""
+          className="flex flex-col w-[70%] md:w-[300px] mx-auto mb-10 p-5 "
+        >
+          <div className="text-center text-[1.7em] font-semibold mb-4">
+            LOGIN
+          </div>
+          <div className="mb-[1%]">Email</div>
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="text-black px-2 py-1 mb-5 rounded-sm bg-opacity-0"
+            type="mail"
+            value={email}
+            placeholder="Enter Email"
+          />
+          <div className="mb-[1%]">Password</div>
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="text-black px-2 py-1 rounded-sm bg-none"
+            type="text"
+            value={password}
+            placeholder="Enter Password"
+          />
+          <div className="my-2 text-[0.8em] hover:text-blue-700">
+            Forget Password?
+          </div>
+          <button type="submit" className="bg-blue-500 py-2 rounded-md">
+            Login
+          </button>
         </form>
+      </div>
     </div>
-    </div>
-    
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
