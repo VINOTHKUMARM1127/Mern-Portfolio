06e34c5a084e87b585dc1324909336042cdb9dce
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-11-11 23:28:07 +0530

    Adding Minor Changes

--- Backend/Routes/UserDataRoute.js
+++ Backend/Routes/UserDataRoute.js
@@ -3,17 +3,16 @@ const router = express.Router();
 const User = require("../Models/Userdata.js");
 
 router.get("/get-user", async (req, res) => {
-    const SecretKey = process.env.SecretKey;
-    if(req.query.key !== SecretKey){
-        return res.status(401).json("Unauthorized Access");
-    }
   try {
+    const SecretKey = process.env.SecretKey;
+    if (req.query.key !== SecretKey) {
+      return res.status(401).json({error:"Unauthorized Access"});
+    }
     const UserList = await User.find();
     res.status(200).json(UserList);
   } catch (err) {
     res.status(400).json(err);
   }
 });
-
 
 module.exports = router;
