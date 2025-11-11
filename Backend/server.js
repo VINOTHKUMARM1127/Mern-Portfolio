4c105c5ac929bd801767c38c07366a57407a19dc
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-11-11 23:16:25 +0530

    Adding Minor Changes

--- Backend/server.js
+++ Backend/server.js
@@ -11,17 +11,17 @@ app.use(cors());
 
 connectdb();
 
-/*const educationRoute = require("./Routes/educationRoute.js");
+//const educationRoute = require("./Routes/educationRoute.js");
 const projectsRoute = require("./Routes/ProjectsRoute.js");
-const UserDataRoute = require("./Routes/UserDataRoute.js");
-const detailsRoute = require("./Routes/detailsRoute.js");
-const SkillsRoute = require("./Routes/SkillsRoute.js");*/
+//const UserDataRoute = require("./Routes/UserDataRoute.js");
+//const detailsRoute = require("./Routes/detailsRoute.js");
+//const SkillsRoute = require("./Routes/SkillsRoute.js");
 
-/*app.use("/", educationRoute);
+//app.use("/", educationRoute);
 app.use("/", projectsRoute);
-app.use("/", UserDataRoute);
-app.use("/", detailsRoute);
-app.use("/", SkillsRoute);*/
+//app.use("/", UserDataRoute);
+//app.use("/", detailsRoute);
+//app.use("/", SkillsRoute);*/
 
 app.get("/", (req,res)=>{
   res.send("Server Running")
